b52f79fa246021b5a7fc42ce57903b93bcddf459
Amirali Eshghi <amir.ali.eshghi@gmail.com>
2023-08-09 00:39:24 +0330

    Add Configuration Service

---
+++ netcore/ServicesRegistration.cs
@@ -0,0 +1,27 @@
+﻿using congestion.calculator.DbContexts;
+using congestion.calculator.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace congestion.calculator
+{
+    public static class ServicesRegistration
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CongestionTaxDbContext>(options =>
+            {
+                options.UseSqlite(configuration.GetConnectionString("CongestionConnectionString"));
+            });
+
+            services.AddScoped<ITollFeeRepository, TollFeeRepository>();
+            services.AddScoped<ITollFreeDateRepository, TollFreeDateRepository>();
+            services.AddScoped<ITollFreeDayOfWeekRepository, TollFreeDayOfWeekRepository>();
+            services.AddScoped<ITollFreeMonthRepository, TollFreeMonthRepository>();
+            services.AddScoped<ITollFreeVehicleRepository, TollFreeVehicleRepository>();
+
+            return services;
+        }
+    }
+}
