a5341fcc54eaef1a8da325ae8198398a36ecc1ab
Amirali Eshghi <amir.ali.eshghi@gmail.com>
2023-08-08 17:17:32 +0330

    Add Automapper Profiles

---
+++ netcore/Profiles/Profiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace congestion.calculator.Profiles
+{
+    public class Profiles : Profile
+    {
+        public Profiles()
+        {
+            CreateMap<Entities.CityEntity, DTOs.CityDto>().ReverseMap();
+
+            CreateMap<Entities.TollFeeEntity, DTOs.TollFeeDto>().ReverseMap();
+
+            CreateMap<Entities.TollFreeDateEntity, DTOs.TollFreeDateDto>().ReverseMap();
+            CreateMap<Entities.TollFreeDayOfWeekEntity, DTOs.TollFreeDayOfWeekDto>().ReverseMap();
+            CreateMap<Entities.TollFreeMonthEntity, DTOs.TollFreeMonthDto>().ReverseMap();
+            CreateMap<Entities.TollFreeVehicleEntity, DTOs.TollFreeVehicleDto>().ReverseMap();
+        }
+    }
+}
