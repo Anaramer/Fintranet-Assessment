cedac9ca05ebabbd279236cd2e46c2d2b3263226
Amirali Eshghi <amir.ali.eshghi@gmail.com>
2023-08-09 09:22:57 +0330

    Create repository of city

---
+++ netcore/Repositories/CityRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using congestion.calculator.DbContexts;
+using congestion.calculator.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace congestion.calculator.Repositories
+{
+    public class CityRepository : ICityRepository
+    {
+        private readonly CongestionTaxDbContext _dbContext;
+
+        public CityRepository(CongestionTaxDbContext congestionTaxDbContext)
+        {
+            _dbContext = congestionTaxDbContext ?? throw new ArgumentException(nameof(CongestionTaxDbContext));
+        }
+
+        public async Task<CityEntity> GetCityByIdAsync(int id)
+        {
+            return await _dbContext.Cities.FirstOrDefaultAsync(q => q.Id == id);
+        }
+
+        public async Task<bool> IsExistAsync(int id)
+        {
+            return await _dbContext.Cities.AnyAsync(q => q.Id == id);
+        }
+    }
+}
---
+++ netcore/Repositories/ICityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using congestion.calculator.Entities;
+
+namespace congestion.calculator.Repositories
+{
+    public interface ICityRepository
+    {
+        Task<CityEntity> GetCityByIdAsync(int id);
+        Task<bool> IsExistAsync(int id);
+    }
+}
